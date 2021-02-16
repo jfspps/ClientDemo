265c3c125db6ba399ebb75fb7c2a2663814ae123
jfspps <jfsapps@gmail.com>
2021-02-16 20:51:38 +0000

    introducing HttpURLConnection class

--- src/com/jfspps/highlevelapi/URL_Main.java
+++ src/com/jfspps/highlevelapi/URL_Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 // this class demonstrates the use of high-level APIs in Java (UDP and TCP classes used previously are low-level)
 public class URL_Main {
@@ -14,7 +16,53 @@ public class URL_Main {
 //        demoURLWithStreams();
 
         // similar to demoURLWithStreams() except that one can set connection parameters before impl the stream
-        demoURLConnectionClass();
+//        demoURLConnectionClass();
+
+        // using HttpURlConnection instead of URLConnection
+        demoHttpURLConnection();
+    }
+
+    private static void demoHttpURLConnection() {
+        System.out.println(" ================== Demo of URLs with HttpURLConnection =====================================");
+
+        try {
+            URL url = new URL("http://google.co.uk");
+
+            // this single connection can be funnelled to multiple objects
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            // set any settings, as with URLConnection
+            httpURLConnection.setRequestMethod("GET");      // GET is the default anyway
+            httpURLConnection.setRequestProperty("User-Agent", "Chrome");
+            httpURLConnection.setReadTimeout(5000);
+
+            // note that getResponse() also performs the connection, hence connect() is redundant
+            // there are other methods which can implicitly call connect()
+            int responseCode = httpURLConnection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200){
+                System.out.println("Error reading page, timeout passed");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(httpURLConnection.getInputStream())
+            );
+
+            String line;
+
+            // checks inputReader.readLine() != null
+            while((line = inputReader.readLine()) != null){
+                System.out.println(line);
+            }
+            inputReader.close();
+
+        } catch (MalformedURLException exception){
+            System.out.println("URL problem: " + exception.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 
     private static void demoURLConnectionClass() {
@@ -33,17 +81,16 @@ public class URL_Main {
             // connect once settings have been configured
             urlConnection.connect();
 
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream())
-            );
-
-            String line = "";
-            while(line != null){
-                line = inputStream.readLine();
-                System.out.println(line);
+            // get the header fields of the URL
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            for(Map.Entry<String, List<String>> entry : headerFields.entrySet()){
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+                System.out.println("Key: " + key);
+                for (String output: value){
+                    System.out.println("Value: " + value);
+                }
             }
-            inputStream.close();
-
         } catch (MalformedURLException exception){
             System.out.println("URL problem: " + exception.getMessage());
         } catch (IOException e) {
